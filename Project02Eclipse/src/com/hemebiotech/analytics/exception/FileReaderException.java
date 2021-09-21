df74890205e472a434cd1b40a6fc4f92cabd5491
Quentin Bruneau <quentinbruneau2508@gmail.com>
2021-09-21 11:03:27 +0200

    Final Version

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/exception/FileReaderException.java
@@ -0,0 +1,9 @@
+package com.hemebiotech.analytics.exception;
+
+import java.io.IOException;
+
+public class FileReaderException extends IOException {
+    public FileReaderException(String msg) {
+        super(msg);
+    }
+}
